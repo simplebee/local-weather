65fc36a1760112bc3e7ee29d94ab3cfafc05712e
Shuiloon Yip <shuoodle@gmail.com>
2016-06-20 20:45:46 +0100

    Add openweathermap ajax request

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-console.log(openWeatherMap.apikey);
+function ajax() {
+  return $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather",
+    data: {
+      q: "london,uk",
+      APPID: openWeatherMap.apikey
+    },
+    type: "GET",
+    datatype: "json"
+  });
+}
+
+function getData(data) {
+  console.log("Success");
+  console.log(data);
+
+  console.log("location", data.name);
+  console.log("time", data.dt);
+
+  console.log("weather icon",data.weather[0].icon);
+  console.log("current temp", data.main.temp);
+
+  console.log("weather description", data.weather[0].description);
+
+  console.log("wind", data.wind.speed);
+  console.log("rain", data.rain["3h"]);
+  console.log("humidity", data.main.humidity);
+}
+
+ajax().done(getData).fail(function() {
+  console.log("Fail");
+});
