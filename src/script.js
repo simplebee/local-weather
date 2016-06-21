c18dc28580580262207d9a569e029451524ea256
Shuiloon Yip <shuoodle@gmail.com>
2016-06-21 20:38:53 +0100

    Add set location to html

--- src/script.js
+++ src/script.js
@@ -13,20 +13,20 @@ function ajax() {
 function getData(data) {
   console.log("Success");
   console.log(data);
-
   console.log("location", data.name);
   console.log("time", data.dt);
-
   console.log("weather icon",data.weather[0].icon);
   console.log("current temp", data.main.temp);
-
   console.log("weather description", data.weather[0].description);
-
   console.log("wind", data.wind.speed);
-  console.log("rain", data.rain["3h"]);
+  // console.log("rain", data.rain["3h"]);
   console.log("humidity", data.main.humidity);
+
+  $("h2").html(data.name);
 }
 
-ajax().done(getData).fail(function() {
-  console.log("Fail");
+$(document).ready(function() {
+  ajax().done(getData).fail(function() {
+    console.log("Fail");
+  });
 });
